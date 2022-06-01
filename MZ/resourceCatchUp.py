bbdc2f4784325f4e4a73de1b0fcbf937ced125a8
Niels <67153942+orchardpark@users.noreply.github.com>
2022-06-01 10:36:34 +0200

    Create resourceCatchUp.py

---
+++ MZ/resourceCatchUp.py
@@ -0,0 +1,12 @@
+import datetime
+def solution(logOut, logIn):
+    start = datetime.datetime.utcfromtimestamp(logOut[0])
+    end = datetime.datetime.utcfromtimestamp(logIn[0])
+    foodDiff = logOut[1]-logIn[1]
+    startDown = datetime.datetime(start.year,start.month, start.day, start.hour, 0, 0)
+    if end.minute == 0 and end.second == 0:
+        endDown = end
+    else:
+        endDown = datetime.datetime(end.year,end.month, end.day, end.hour, 0, 0)+datetime.timedelta(hours=1)
+    
+    return foodDiff / ((endDown-startDown).total_seconds()/3600 - 1
